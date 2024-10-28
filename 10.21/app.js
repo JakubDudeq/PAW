586a644dc87541a9532a98aaf56535f91014d5e4
Dudek <126721457+JakubDudeq@users.noreply.github.com>
2024-10-28 16:05:00 +0100

    Add files via upload

--- 10.21/app.js
+++ 10.21/app.js
@@ -11,7 +11,7 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-  // view engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -24,12 +24,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-  // catch 404 and forward to error handler
+app.post('/kontaktForm', (req, res) => {
+  const { inp1, inp2, inp3, inp4 } = req.body;
+
+  console.log(`Imię: ${inp1}`);
+  console.log(`Nazwisko: ${inp2}`);
+  console.log(`Email: ${inp3}`);
+  console.log(`Treść wiadomości: ${inp4}`);
+
+  res.redirect('/');
+
+});
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-  // error handler
+
+
+// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -40,17 +54,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.post('/kontaktForm', (req, res) => {
-  const { inp1, inp2, inp3, inp4 } = req.body;
-
-  // Wyświetlenie danych w konsoli
-  console.log(`Imię: ${inp1}`);
-  console.log(`Nazwisko: ${inp2}`);
-  console.log(`Email: ${inp3}`);
-  console.log(`Treść wiadomości: ${inp4}`);
-
-  // Przekierowanie na stronę główną
-  res.redirect('/');
-});
 
 module.exports = app;
